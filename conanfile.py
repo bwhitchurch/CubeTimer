28d63f8d6dcbdb99949dcbd3009a60e95a33682f
Brandon Michael Whitchurch <brandonwhitchurch@gmail.com>
2023-01-15 15:16:01 -0800

    Added CLI11 dependency and changed conan generator to cmake_multi_find_package

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import CMake, ConanFile
 
 class cube_timer_conan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "fmt/9.1.0", "spdlog/1.11.0", "catch2/3.2.1"
-    generators = "cmake", "gcc", "txt"
+    requires = "fmt/9.1.0", "spdlog/1.11.0", "catch2/3.2.1", "cli11/2.3.2"
+    generators = "cmake_find_package_multi"
     default_options = {"fmt:header_only": True, "spdlog:header_only": True,
                        "catch2:fPIC": True, "catch2:shared": True}
