7cdc982ca5db47d0e6793e5cbe44125011407252
Brandon Michael Whitchurch <brandonwhitchurch@gmail.com>
2023-01-15 18:41:19 -0800

    Barebones CLI and logger skeleton.

--- app/cubeTimer++.cpp
+++ app/cubeTimer++.cpp
@@ -1,12 +1,36 @@
-#include "fmt/format.h"
+#include <CLI/CLI.hpp>
+#include <internal_use_only/config.hpp>
 #include <iostream>
+#include <spdlog/spdlog.h>
 void sayHello();
-int main() {
-    try{
-        fmt::print("Hello World!\n");
-        sayHello();
-    } catch (...) {
-        std::cout << "An exception occurred" <<std::endl;
-    }
-    return 0;
+
+// NOLINTNEXTLINE(bugprone-excetpion-escape)
+int main(int argc, char** argv) {
+	try {
+		CLI::App app{
+            fmt::format("{} version {}",
+			cubetimer::cmake::project_name,
+			cubetimer::cmake::project_version)};
+		bool print_version = false;
+		app.add_flag(
+			"--version", print_version, "Print version information and exit."
+		);
+
+		CLI11_PARSE(app, argc, argv);
+
+		if (print_version) {
+			fmt::print(
+				"{} version {}\nCompiled from: {}",
+				cubetimer::cmake::project_name,
+				cubetimer::cmake::project_version,
+				cubetimer::cmake::git_sha
+			);
+			return EXIT_SUCCESS;
+		}
+		spdlog::info("Hello World!");
+		sayHello();
+	} catch (const std::exception& e) {
+		spdlog::error("Unhandled exception in main: {}", e.what());
+	}
+	return EXIT_SUCCESS;
 }
